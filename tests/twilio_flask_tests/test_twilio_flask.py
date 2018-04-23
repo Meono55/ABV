2c1271b7c0071b71acdfa354c6626cfb48b6b80c
Jose Meono <jose.a.meono@gmail.com>
2018-04-23 11:07:47 -0400

    Added tests to twilio flask to ensure proper TDD

--- tests/twilio_flask_tests/test_twilio_flask.py
+++ tests/twilio_flask_tests/test_twilio_flask.py
@@ -1,43 +1,54 @@
-import abv.twilio_api.twilio_flask as twilio_flask
-from pytest import fixture
-from requests_mock import Mocker
 import re
+from pytest import fixture
+import abv.twilio_api.twilio_flask as twilio_flask
+import requests_mock
+
 
 
 @fixture()
 def app():
     twilio_flask.app.testing = True
-    app = twilio_flask.app.test_client()
-    return app
+    inner_app = twilio_flask.app.test_client()
+    return inner_app
 
 
+# pylint: disable=redefined-outer-name
+def test_bad_status_code(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[]', status_code=404)
+        result = app.get('/', data={'Body':'stout'})
+        assert b'Sorry, I cannot handle your request. Please try again later!' in result.data
+
+
+# pylint: disable=redefined-outer-name
 def test_valid_twilio_format(app):
-    with Mocker() as m:
+    with requests_mock.Mocker() as m:
         m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}, {"name":"foo"}]')
         result = app.get('/', data={'Body': 'stout'})
         assert result.status_code == 200
         assert result.data.startswith(b'<?xml')
 
 
+# pylint: disable=redefined-outer-name
 def test_no_results(app):
-    with Mocker() as m:
+    with requests_mock.Mocker() as m:
         m.register_uri('GET', re.compile('beerapi'), text='[]')
-        result = app.get('/', data = {'Body':'stout'})
+        result = app.get('/', data={'Body':'stout'})
         assert b'Sorry, no results for stout' in result.data
 
-
+# pylint: disable=redefined-outer-name
 def test_one_result(app):
-    with Mocker() as m:
+    with requests_mock.Mocker() as m:
         m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}]')
         result = app.get('/', data={'Body': 'porter'})
         assert b'There is 1 beer with the style porter' in result.data
 
+# pylint: disable=redefined-outer-name
 def test_tw0_result(app):
-    with Mocker() as m:
+    with requests_mock.Mocker() as m:
         m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}, {"name":"bar"}]')
         result = app.get('/', data={'Body': 'IPA'})
         assert b'There are 2 beers with the style IPA' in result.data
-
 
 
 def test_count_beers():
